a3b9d5be3db94ae8f7cd049558709b0f0bddc299
Szybet <53944559+Szybet@users.noreply.github.com>
2025-05-03 13:29:50 +0000

    kind of working rotation

--- src/embedded_graphics.rs
+++ src/embedded_graphics.rs
@@ -1,5 +1,4 @@
 use embedded_graphics_core::prelude::{Dimensions, DrawTarget, PixelColor};
-
 use super::*;
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
@@ -24,17 +23,10 @@ where
     [(); FB_WIDTH * FB_HEIGHT / 8]:,
 {
     pub fn new(color: Color) -> Self {
-        // https://github.com/nvzqz/static-assertions/issues/40
-        // Not yet
-        // assert!(FB_WIDTH % 8 == 0);
-        // assert!(FB_HEIGHT % 8 == 0);
-        // This should be handled in another way
-
         let color_byte = match color {
             Color::Black => 0,
             Color::White => 0xFF,
         };
-
         Self {
             framebuffer: [color_byte; FB_WIDTH * FB_HEIGHT / 8],
         }
@@ -46,14 +38,29 @@ where
         x_lo: i16,
         y_lo: i16,
     ) -> Result<(), Error<C>> {
+        let mut rotated: [u8; FB_WIDTH * FB_HEIGHT / 8] = [0; FB_WIDTH * FB_HEIGHT / 8];
+        for y in 0..FB_HEIGHT {
+            for x in 0..FB_WIDTH {
+                let src_idx = (y * FB_WIDTH + x) / 8;
+                let bit_val = (self.framebuffer[src_idx] >> (7 - (x % 8))) & 1;
+                if bit_val == 1 {
+                    // 90° CCW: (x,y) → (new_x, new_y) = (y, FB_WIDTH−1−x)
+                    let new_x = y;
+                    let new_y = FB_WIDTH - 1 - x;
+                    let dst_idx = (new_y * FB_HEIGHT + new_x) / 8;
+                    let dst_bit = 7 - (new_x % 8);
+                    rotated[dst_idx] |= 1 << dst_bit;
+                }
+            }
+        }
         display.draw_image(
-            &self.framebuffer,
+            &rotated,
             x_lo,
             y_lo,
-            x_lo + FB_WIDTH as i16,
-            y_lo + FB_HEIGHT as i16,
+            x_lo + FB_HEIGHT as i16,
+            y_lo + FB_WIDTH as i16,
         )
-    }    
+    }
 }
 
 impl<const FB_WIDTH: usize, const FB_HEIGHT: usize> Dimensions
@@ -65,8 +72,8 @@ where
         embedded_graphics_core::primitives::Rectangle {
             top_left: embedded_graphics_core::geometry::Point { x: 0, y: 0 },
             size: embedded_graphics_core::geometry::Size {
-                width: FB_WIDTH as u32,
-                height: FB_HEIGHT as u32,
+                width: FB_HEIGHT as u32,
+                height: FB_WIDTH as u32,
             },
         }
     }
@@ -85,27 +92,20 @@ where
         I: IntoIterator<Item = embedded_graphics_core::Pixel<Color>>,
     {
         for embedded_graphics_core::Pixel(point, color) in pixels {
-            // Maybe implement it for the enum?
             let color_value = match color {
                 Color::White => 1,
                 Color::Black => 0,
             };
-
             let x = point.x as usize;
             let y = point.y as usize;
-
             if x >= FB_WIDTH || y >= FB_HEIGHT {
                 continue;
             }
-
             let index = (y * FB_WIDTH + x) / 8;
             let bit = 7 - (x % 8);
-
             if color_value == 1 {
-                // White
                 self.framebuffer[index] |= 1 << bit;
             } else {
-                // Black
                 self.framebuffer[index] &= !(1 << bit);
             }
         }
